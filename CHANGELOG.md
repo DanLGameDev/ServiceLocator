57f5b19407f9255519d731a3922f5ff4093865ac
Daniel Loughmiller <dan@effectwebagency.com>
2024-10-03 10:16:51 -0400

    1.0.9 - services cleared when exiting play mode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
 # Changelog
 
+## [1.0.9] - 2024-10-03
+### Changes
+- Services will now be cleared when exiting play mode
+
 ## [1.0.8] - 2024-09-10
-### Changed
+### Changes
 - `LocateService` will now throw an exception if the service is not found.
 
 ## [1.0.0] - 2024-08-30
---
+++ Editor/ServiceLocatorEditorUtil.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+
+namespace DGP.ServiceLocator.Editor
+{
+    public class ServiceLocatorEditorUtil
+    {
+        
+        [InitializeOnLoadMethod]
+        private static void Initialize() {
+            EditorApplication.playModeStateChanged -= PlayModeStateChange;
+            EditorApplication.playModeStateChanged += PlayModeStateChange;
+        }
+
+        private static void PlayModeStateChange(PlayModeStateChange obj) {
+            if (obj == UnityEditor.PlayModeStateChange.ExitingPlayMode) {
+                ServiceLocator.ClearServices();
+            }
+        }
+    }
+}
